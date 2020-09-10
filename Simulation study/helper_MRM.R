b7afd02406c320223cb4727b2e74ff8dec58650d
Maya Mathur <mmathur@stanford.edu>
2020-09-10 07:53:23 -0700

    use my_boot to catch sporadic boot failures

--- Simulation study/helper_MRM.R
+++ Simulation study/helper_MRM.R
@@ -107,6 +107,9 @@ prop_stronger_mr = function(dat,
   Zc = dat$Zc
   Zb = dat$Zb
   
+  bootFail = FALSE
+  BtCorrectNote = NA
+  
   
   if ( calib.method != "params" ) {
     # fit meta-regression
@@ -139,30 +142,37 @@ prop_stronger_mr = function(dat,
   
   ##### One-Stage Calibration Method ("MR") #####
   # directly use the meta-regressive estimates in Equation (1)
-  if ( calib.method == "MR" ) {
+  if ( calib.method %in% c("MR", "params") ) {
     ens.shift = c(bhat0) + sqrt( c(t2) / ( c(t2) + vyi) ) * ( dat$yi.shift - c(bhat0) )
   }
   
-  
+  #browser()
+  #bm
+
   if ( calib.method %in% c("MR bt mn correct", "MR bt var correct", "MR bt both correct" ) ) {
     
     tryCatch({
-      boot.res = boot( data = dat, 
+      boot.res = my_boot( data = dat, 
                        parallel = "multicore",
                        R = boot.reps, 
                        statistic = function(original, indices) {
                          b = dat[indices,]
                          
-                         mb = robu( yi ~ Zc + Zb, 
-                                    data = b, 
-                                    studynum = 1:nrow(b),
-                                    var.eff.size = vyi )
-                         bhat0.bt = mb$b.r[1]
-                         bhatc.bt = mb$b.r[2]
-                         bhatb.bt = mb$b.r[3]
-                         t2.bt = mb$mod_info$tau.sq
-                         
-                         return( c(bhat0.bt, bhatc.bt, bhatb.bt, t2.bt) )
+                         tryCatch({
+                           mb = robu( yi ~ Zc + Zb, 
+                                      data = b, 
+                                      studynum = 1:nrow(b),
+                                      var.eff.size = vyi )
+                           bhat0.bt = mb$b.r[1]
+                           bhatc.bt = mb$b.r[2]
+                           bhatb.bt = mb$b.r[3]
+                           t2.bt = mb$mod_info$tau.sq
+                           
+                           return( c(bhat0.bt, bhatc.bt, bhatb.bt, t2.bt) )
+                         }, error = function(err){
+                           return( rep(NA, 4) )
+                         })
+                        
                        } ) # end boot()
       
       bt.means = as.numeric( colMeans( boot.res$t, na.rm = TRUE ) )
@@ -192,12 +202,16 @@ prop_stronger_mr = function(dat,
       bhatc <<- NA 
       bhatb <<- NA 
       t2 <<- NA
+      BtCorrectNote <<- err$message
+      bootFail <<- TRUE
+
     })
     
   }  # end of calib.method == "MR bt mn correct"
   
+
   # ens.shift might not exist if we were trying to bootstrap-correct it but the bootstrapping failed
-  if ( exists("ens.shift") ) {
+  if ( bootFail == FALSE ) {
     # q shifted to set moderators to 0
     q.shift = p$q - (bhatc * zc.star) - (bhatb * zb.star) # remove intercept from sum(m$b.r)
     # @NOTE: ASSUMES TAIL = ABOVE
@@ -225,7 +239,8 @@ prop_stronger_mr = function(dat,
                         bhat0 = bhat0,
                         bhatc = bhatc, 
                         bhatb = bhatb, 
-                        t2 = t2) )
+                        t2 = t2,
+                        BtCorrectNote = BtCorrectNote) )
     
   }
 }
@@ -1086,6 +1101,164 @@ stitch_files = function(.results.singles.path, .results.stitched.write.path=.res
   write.csv(s, paste(.results.stitched.write.path, .stitch.file.name, sep="/") )
   return(s)
 }
+
+
+################################### MODIFIED FROM BOOT PACKAGE ################################### 
+
+# see section called "MM additions"
+# I minimally modified the function so that it can proceed even if some of the bootstrap iterates run into errors
+# (in this case, Fisher convergence issues) because the boot package version gets confused about dimension mismatches
+
+# source internal boot package functions
+# source("bootfuns.R")
+
+my_boot = function (data, statistic, R, sim = "ordinary", stype = c("i", 
+                                                                    "f", "w"), strata = rep(1, n), L = NULL, m = 0, weights = NULL, 
+                    ran.gen = function(d, p) d, mle = NULL, simple = FALSE, ..., 
+                    parallel = c("no", "multicore", "snow"), ncpus = getOption("boot.ncpus", 
+                                                                               1L), cl = NULL) 
+{
+  
+  
+  call <- match.call()
+  stype <- match.arg(stype)
+  if (missing(parallel)) 
+    parallel <- getOption("boot.parallel", "no")
+  parallel <- match.arg(parallel)
+  have_mc <- have_snow <- FALSE
+  if (parallel != "no" && ncpus > 1L) {
+    if (parallel == "multicore") 
+      have_mc <- .Platform$OS.type != "windows"
+    else if (parallel == "snow") 
+      have_snow <- TRUE
+    if (!have_mc && !have_snow) 
+      ncpus <- 1L
+    loadNamespace("parallel")
+  }
+  if (simple && (sim != "ordinary" || stype != "i" || sum(m))) {
+    warning("'simple=TRUE' is only valid for 'sim=\"ordinary\", stype=\"i\", n=0', so ignored")
+    simple <- FALSE
+  }
+  if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
+    runif(1)
+  seed <- get(".Random.seed", envir = .GlobalEnv, inherits = FALSE)
+  n <- NROW(data)
+  if ((n == 0) || is.null(n)) 
+    stop("no data in call to 'boot'")
+  temp.str <- strata
+  strata <- tapply(seq_len(n), as.numeric(strata))
+  t0 <- if (sim != "parametric") {
+    if ((sim == "antithetic") && is.null(L)) 
+      L <- empinf(data = data, statistic = statistic, stype = stype, 
+                  strata = strata, ...)
+    if (sim != "ordinary") 
+      m <- 0
+    else if (any(m < 0)) 
+      stop("negative value of 'm' supplied")
+    if ((length(m) != 1L) && (length(m) != length(table(strata)))) 
+      stop("length of 'm' incompatible with 'strata'")
+    if ((sim == "ordinary") || (sim == "balanced")) {
+      if (isMatrix(weights) && (nrow(weights) != length(R))) 
+        stop("dimensions of 'R' and 'weights' do not match")
+    }
+    else weights <- NULL
+    if (!is.null(weights)) 
+      weights <- t(apply(matrix(weights, n, length(R), 
+                                byrow = TRUE), 2L, normalize, strata))
+    if (!simple) 
+      i <- index.array(n, R, sim, strata, m, L, weights)
+    original <- if (stype == "f") 
+      rep(1, n)
+    else if (stype == "w") {
+      ns <- tabulate(strata)[strata]
+      1/ns
+    }
+    else seq_len(n)
+    t0 <- if (sum(m) > 0L) 
+      statistic(data, original, rep(1, sum(m)), ...)
+    else statistic(data, original, ...)
+    rm(original)
+    t0
+  }
+  else statistic(data, ...)
+  pred.i <- NULL
+  fn <- if (sim == "parametric") {
+    ran.gen
+    data
+    mle
+    function(r) {
+      dd <- ran.gen(data, mle)
+      statistic(dd, ...)
+    }
+  }
+  else {
+    if (!simple && ncol(i) > n) {
+      pred.i <- as.matrix(i[, (n + 1L):ncol(i)])
+      i <- i[, seq_len(n)]
+    }
+    if (stype %in% c("f", "w")) {
+      f <- freq.array(i)
+      rm(i)
+      if (stype == "w") 
+        f <- f/ns
+      if (sum(m) == 0L) 
+        function(r) statistic(data, f[r, ], ...)
+      else function(r) statistic(data, f[r, ], pred.i[r, 
+      ], ...)
+    }
+    else if (sum(m) > 0L) 
+      function(r) statistic(data, i[r, ], pred.i[r, ], 
+                            ...)
+    else if (simple) 
+      function(r) statistic(data, index.array(n, 1, sim, 
+                                              strata, m, L, weights), ...)
+    else function(r) statistic(data, i[r, ], ...)
+  }
+  RR <- sum(R)
+  res <- if (ncpus > 1L && (have_mc || have_snow)) {
+    if (have_mc) {
+      parallel::mclapply(seq_len(RR), fn, mc.cores = ncpus)
+    }
+    else if (have_snow) {
+      list(...)
+      if (is.null(cl)) {
+        cl <- parallel::makePSOCKcluster(rep("localhost", 
+                                             ncpus))
+        if (RNGkind()[1L] == "L'Ecuyer-CMRG") 
+          parallel::clusterSetRNGStream(cl)
+        res <- parallel::parLapply(cl, seq_len(RR), fn)
+        parallel::stopCluster(cl)
+        res
+      }
+      else parallel::parLapply(cl, seq_len(RR), fn)
+    }
+  }
+  else lapply(seq_len(RR), fn)
+  #t.star <- matrix(, RR, length(t0))  # ~~~ MM commented out
+  
+  
+  # ~~~~~ MM added
+  # number of non-NULL elements of the results vector
+  #browser()
+  #RR = length(unlist(res))
+  # changed to accommodate multi-argument returns:
+  RR = length(res)
+  nulls = sapply( res, is.null)
+  res = res[ !nulls ]
+  t.star <- matrix(, RR, length(t0))
+  
+  # without this, boot.CI gets confused about number of replicates
+  R = RR
+  # ~~~~~ end of MM additions
+  
+  
+  for (r in seq_len(RR)) t.star[r, ] <- res[[r]]
+  if (is.null(weights)) 
+    weights <- 1/tabulate(strata)[strata]
+  boot.return(sim, t0, t.star, temp.str, R, data, statistic, 
+              stype, call, seed, L, m, pred.i, weights, ran.gen, mle)
+}
+
 
 
 
